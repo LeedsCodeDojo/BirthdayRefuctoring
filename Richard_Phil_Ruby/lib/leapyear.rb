1affa9a92eb34e3402aa13fc5d7302572313fd9a
Phil Helliwell <phil.helliwell@neon.me.uk>
2016-06-02 22:24:57 +0100

    Richard_Phil_Ruby | Logic reversal

--- Richard_Phil_Ruby/lib/leapyear.rb
+++ Richard_Phil_Ruby/lib/leapyear.rb
@@ -1,15 +1,22 @@
 class LeapYear
   # 0. Reference implementation
+  # 1. Logic reversal
 
   def leap?(year)
+    !is_common_year?(year)
+  end
+
+private
+
+  def is_common_year?(year)
     if (year % 4 != 0)
-      false
+      true
     elsif (year % 100 != 0)
-      true
-    elsif (year % 400 != 0)
       false
-    else
+    elsif (year % 400 != 0)
       true
+    else
+      false
     end
   end
 
